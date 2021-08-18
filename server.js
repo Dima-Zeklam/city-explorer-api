d45ea195562cf1d116d9724d28439ea294d834ef
Dima Zeklam <dema.ziklam@gmail.com>
2021-08-18 11:05:58 +0300

    backend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(cors());
 
 
 server.get('/', (req, res) => {
-    res.send('no error, you in home page');
+    res.send('ALL GOOD');
 
 });
 
@@ -24,22 +24,11 @@ class forcast {
     }
 }
 
-class Movie {
-    constructor(item) {
-        this.title = item.title;
-        this.overview = item.overview;
-        this.average_votes = item.vote_average;
-        this.total_votes = item.vote_count;
-        this.image_url = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
-        this.popularity = item.popularity;
-        this.released_on = item.release_date;
-
-    }
-}
+//MOVIE_API_KEY
+// https://api.themoviedb.org/3/movie/550?api_key=3e93ed962f0f458c5c241108f065a8ec
 
 
 server.get('/weather', (req, res) => {
-
 
 //localhost:3001/weather?searchQuery=amman&lat=31&lon=35
     let cityName = req.query.searchQuery;
@@ -60,33 +49,6 @@ server.get('/weather', (req, res) => {
         return res.status(500).send('this city is not found');
     }
 });
-
-server.get('/movie', (req, res) => {
-
-
-//https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}
-
-        let cityName = req.query.searchQuery;
-        let lat = req.query.lat;
-        let lon = req.query.lon;
-        let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`;
-        try{
-        axios.get(movieUrl).then(movie => {
-            let movieData = movie.data.results.map(item=>{
-          let movieCity = new Movie(item);
-             return movieCity;
-            })
-            res.send(movieData);
-     
-        });
-        console.log('wetherData is ',weatherData);
-            console.log(forcastArr);
-           
-        } catch (error) {
-            return res.status(500).send('ERROR');
-        }
-    });
-
 
 
 server.get('*', (req, res) => {
