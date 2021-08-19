ab83e7c29767ddf585e8ee570e9ff9b72c912cfe
Dima Zeklam <dema.ziklam@gmail.com>
2021-08-19 15:28:42 +0300

    increase the performance

--- modules/movie.js
+++ modules/movie.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-
+let inMemory ={};
 
 let getMovieData = (req, res) => {
 
@@ -13,7 +13,14 @@ let getMovieData = (req, res) => {
     let cityMovieName = req.query.city;
     console.log('cityName is ', cityMovieName);
     let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityMovieName}`;
-    // try {
+
+if(inMemory[cityMovieName] !== undefined){
+    console.log('cache hit, data in cache memory');
+    res.send(inMemory[cityMovieName]);
+}
+else{
+    try {
+        console.log('cache miss , send req to API');
         axios.get(movieUrl).then(ele => {
             let movieData = ele.data.results.map(item => {
 
@@ -28,23 +35,24 @@ let getMovieData = (req, res) => {
 
 
 
-    // } catch (error) {
-    //     return res.status(404).send('not found');
-    // }
+    } catch (error) {
+        return res.status(404).send('not found');
+    }}
 
-    class movie {
-        constructor(dataItem) {
-            this.title = dataItem.title;
-            this.overview = dataItem.overview;
-            this.average_votes = dataItem.vote_average;
-            this.total_votes = dataItem.vote_count;
-             this.image_url = `https://image.tmdb.org/t/p/w500${dataItem.poster_path}` ;
-            this.popularity = dataItem.popularity;
-            this.released_on = dataItem.release_date;
-           
-    
-        }
-    }
+
 }
 
+class movie {
+    constructor(dataItem) {
+        this.title = dataItem.title;
+        this.overview = dataItem.overview;
+        this.average_votes = dataItem.vote_average;
+        this.total_votes = dataItem.vote_count;
+         this.image_url = `https://image.tmdb.org/t/p/w500${dataItem.poster_path}` ;
+        this.popularity = dataItem.popularity;
+        this.released_on = dataItem.release_date;
+       
+
+    }
+}
 module.exports = getMovieData;
